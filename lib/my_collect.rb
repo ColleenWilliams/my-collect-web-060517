61198718cc89279100ca220f11398f1b8a08e775
github username <github email address>
2017-05-22 13:54:39 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(arrays_for_days)
+  i = 0
+  new_array = []
+  while i <= (arrays_for_days.count - 1)
+    new_array.push(yield arrays_for_days[i])
+    i+=1
+  end
+  new_array
+end
